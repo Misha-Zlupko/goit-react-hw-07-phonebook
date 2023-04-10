ec6afd4d122f157d1c5d4297f2f16d493d8c251d
mafioz359 <64733979+mafioz359@users.noreply.github.com>
2023-04-10 16:44:35 +0300

    d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
 import { Feedbacks } from './FeedBack/FeedBack';
-import { getError, getItem, getLoading } from 'redux/selectors';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { getError, getLoading } from 'redux/selectors';
+import { useSelector } from 'react-redux';
 
 export const App = () => {
-  const dispatch = useDispatch();
   const isLoading = useSelector(getLoading);
   const error = useSelector(getError);
-
-  // useEffect(() => {
-  //   dispatch(getItem());
-  // }, [dispatch]);
 
   return (
     <div>
